a50c7fd3256d2c58719dbadb95867cac26f52cd9
Aleksandr Matrosov <unglud@gmail.com>
2021-05-05 16:11:17 +0200

    detail

--- pages/[meetupId].js
+++ pages/[meetupId].js
@@ -1,39 +1,55 @@
-import MeetupDetail from "../components/meetups/MeetupDetail";
+import MeetupDetail from '../components/meetups/MeetupDetail';
+import {MongoClient, ObjectId} from 'mongodb';
 
 export default function MeetupDetailsPage (props) {
-    return <MeetupDetail props={props.meetupData}/>;
+    return (
+        <MeetupDetail
+            image={props.meetupData.image}
+            title={props.meetupData.title}
+            address={props.meetupData.address}
+            description={props.meetupData.description}
+        />
+    );
 }
 
 export async function getStaticPaths () {
+    const client = await MongoClient.connect(process.env.MONGODB_HOST);
+
+    const db = client.db();
+    const meetupsCollection = db.collection('meetups');
+
+    const meetups = await meetupsCollection.find({}, {_id: 1}).toArray();
+    client.close();
     return {
         fallback: false,
-        paths: [
-            {
-                params: {
-                    meetupId: 'm1'
-                }
-            },
-            {
-                params: {
-                    meetupId: 'm2'
-                }
+        paths: meetups.map((meetup) => ({
+            params: {
+                meetupId: meetup._id.toString()
             }
-        ]
-    }
+        }))
+    };
 }
 
 export async function getStaticProps (context) {
     const meetupId = context.params.meetupId;
 
-    console.log(`meetupId`, meetupId);
+    const client = await MongoClient.connect(process.env.MONGODB_HOST);
 
+    const db = client.db();
+    const meetupsCollection = db.collection('meetups');
+
+    const meetupData = await meetupsCollection.findOne({
+        _id: ObjectId(meetupId)
+    });
+
+    client.close();
     return {
         props: {
             meetupData: {
-                id: "m1",
-                title: "First Meetup",
-                image: "https://source.unsplash.com/random",
-                address: "some address",
+                id: meetupData._id.toString(),
+                title: meetupData.title,
+                address: meetupData.address,
+                image: meetupData.image
             }
         }
     }
